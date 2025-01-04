ed5e2774a1bb82ade2bcd28bac7ac36c42c86663
Pratik Patil <90708235+github-pratik@users.noreply.github.com>
2025-01-03 21:13:44 -0500

    Updates

--- precache-manifest.096e4f4f38b0c8282b8f58cfdacec78c.js
+++ precache-manifest.096e4f4f38b0c8282b8f58cfdacec78c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59dc6a5d52dd0ec8666babd6bf5b0c91",
+    "revision": "d4a04041e604c2059631770e6280ca2d",
     "url": "/API-Gateway-with-Microservices/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/API-Gateway-with-Microservices/static/js/2.a8dcf01b.chunk.js"
   },
   {
-    "revision": "85e5c09df46a699d7d0d",
-    "url": "/API-Gateway-with-Microservices/static/js/main.5033b10c.chunk.js"
+    "revision": "d070062c38e467a4ab9e",
+    "url": "/API-Gateway-with-Microservices/static/js/main.bd9d8ca2.chunk.js"
   },
   {
     "revision": "5a6476b85761fc556e05",
